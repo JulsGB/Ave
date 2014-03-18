3d537d5edea24c976c8af849b17e038d80b32a6b
JulsGB <julio.granados.barros@gmail.com>
2014-03-18 17:41:51 +0100

    Primeros cambios para los videos

--- Admin.php
+++ Admin.php
@@ -1,61 +1,76 @@
 <?php
-$login=$_POST["usuario"];
-$pass=$_POST["password"];
-
+$login = $_POST["usuario"];
+$pass = $_POST["password"];
 ?>
 
 <!DOCTYPE html>         
 <html>
-    
+
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
         <title> Admin</title>
         <meta charset="utf-8">
-            
+
     </head>
     <body>
         <form method="post" enctype="multipart/form-data" action="AnyadirAdmin.php">        
-        <input type=file size=60 name="imagen">
-       
-        
-        
-        <?
+            
+
+
+
+            <?
             require_once("./Usuario.php");
             require_once("./Estacion.php");
-            $usuario= new Usuario();
+            $usuario = new Usuario();
             $usuario->setLogin($login);
             $usuario->setPassword($pass);
-            $seguro=$usuario->Autenticar($usuario->getLogin(), $usuario->getPassword());
-            
-            if($seguro){
-                $estacion=new Estacion();
-                $ciudad=new Estacion();
-                $ciudades=$estacion->DameNombreCiudades();
-               echo'<label> Ciudad</label>' ;
-               echo' <SELECT NAME="Ciudad">'; 
-                   
-               foreach ($ciudades as $ciudad) {
-                     echo "<option  value= \"" . $ciudad->getCiudad() . "\">" . $ciudad->getCiudad() . "</option>";
-               }     
-               echo'</SELECT>'; 
-            }
-            else{
-                
+            $seguro = $usuario->Autenticar($usuario->getLogin(), $usuario->getPassword());
+
+            if ($seguro) {
+                $estacion = new Estacion();
+                $ciudad = new Estacion();
+                $ciudades = $estacion->DameNombreCiudades();
+              ?> 
+                 <table>
+
+                <tr>
+                    <td><?echo'<label> Ciudad</label>';?></td>
+                    <td><?echo' <SELECT NAME="Ciudad">';
+
+                       foreach ($ciudades as $ciudad) {
+                       echo "<option  value= \"" . $ciudad->getCiudad() . "\">" . $ciudad->getCiudad() . "</option>";
+                        }
+                        echo'</SELECT>';?>
+                    </td>
+                    <td><input type=file size=60 name="imagen"></td>
+                    <td> <input type="submit" value="SubirImagen"></td>
+                </tr>
+                <tr>
+                    <td><?echo'<label>Trayecto</label>';?></td>
+                    <td><?echo' <SELECT NAME="Ciudad">';
+
+                       foreach ($ciudades as $ciudad) {
+                       echo "<option  value= \"" . $ciudad->getCiudad() . "\">" . $ciudad->getCiudad() . "</option>";
+                        }
+                        echo'</SELECT>';?>
+                    </td>
+                    <td><input type=file size=60 name="video"></td>
+                    <td> <input type="submit" value="SubirVideo"></td>
+                </tr>
+            </table>
+
+            <?    
+            } else {
+
                 echo"<script>
                     alert('usuario o contraseña incorrectos');
                      </script>";
-                
+
                 echo"<script>window.location='Home.php'</script>";
-                         
-                    
-                
-                
-                }
-                
-        
-        ?>
-        <input type="submit" value="Subir">
-       </form>
+            }
+            ?>
+           
+        </form>
     </body>
 
 
---
+++ BDTrayecto.php
@@ -0,0 +1,38 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of BDTrayecto
+ *
+ * @author Samsung
+ */
+
+require_once ("./BDatosConexion.php");
+class BDTrayecto {
+    private $BD = null;
+
+    //creamos una un objeto de base de datos
+    public function __construct() {
+        $this->BD = new BDatosConexion();
+    }
+
+    public function __destruct() {
+        
+    }
+    
+    public function DameTrayecto($ciudadOrigen,$ciudadDestino){
+       $sql= "select Estacion_Ciudad_id_Origen, Estacion_Ciudad_id_Destino from Trayecto group by Estacion_Ciudad_id_Origen, Estacion_Ciudad_id_Destino;";
+    
+    }
+    
+    public function DameCiudad($ciudad){
+     
+        return $nombre;
+    }   
+    }
+
+?>
